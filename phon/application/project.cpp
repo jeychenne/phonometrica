547af19f6ae671cc08deb4c9d76db8b7e7dff59c
eychenne <julien.eychenne@usherbrooke.ca>
2026-03-28 09:06:03 -0400

    fix scoring in Weenink's method

--- phon/application/project.cpp
+++ phon/application/project.cpp
@@ -54,7 +54,7 @@ Project::Project(Runtime &rt, String path) :
 		m_corpus(make_handle<Directory>(nullptr, "Corpus")),
 		m_bookmarks(make_handle<Directory>(nullptr, "Bookmarks")),
 		m_scripts(make_handle<Directory>(nullptr, "Scripts")),
-        m_data(make_handle<Directory>(nullptr, "Data tables")),
+        m_data(make_handle<Directory>(nullptr, "Datasets")),
 		m_queries(make_handle<Directory>(nullptr, "Queries"))
 {
 	if (path.empty())
