2d430e2b92f2ae97b4c0d6b3a7086663fed23c12
julien <julien.eychenne@usherbrooke.ca>
2026-09-09 12:52:36 -0400

    update Qt version in mac deploy script

--- deploy_mac.sh
+++ deploy_mac.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 # Adapt to your system
-VERSION="6.11.0"
+VERSION="6.11.1"
 OUTDIR="$HOME/Temp/build/phon-release"
 
 # This assumes a standard install in the user's home directory
